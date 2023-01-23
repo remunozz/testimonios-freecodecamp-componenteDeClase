5bb689cf3e3f153d0d4f5b7edcb182cca4fd2029
remunozz <87038583+remunozz@users.noreply.github.com>
2023-01-23 18:27:01 -0500

    Componente funcional Testimonio.jsx

--- src/componentes/Testimonio.jsx
+++ src/componentes/Testimonio.jsx
@@ -1,26 +1,24 @@
 import React from 'react';
 import '../hojas-de-estilo/Testimonio.css';
 
-class Testimonio extends React.Component {
-  render() {
-    return (
+function Testimonio (props){
+  return (
       <div className = 'contenedor-testimonio'>
           <img 
               className = 'imagen-testimonio'
-              src={require(`../imagenes/testimonio-${this.props.imagen}.png`)}
-              alt={`Foto de ${this.props.nombre}`} />
+              src={require(`../imagenes/testimonio-${props.imagen}.png`)}
+              alt={`Foto de ${props.nombre}`}/>
           <div className='contenedor-texto-testimonio'>
               <p className = 'nombre-testimonio'>
-                  <strong>{this.props.nombre}</strong> en {this.props.pais}
+                  <strong>{props.nombre}</strong> en {props.pais}
               </p>
               <p className = 'cargo-testimonio'>
-                  {this.props.cargo} en <strong>{this.props.empresa}</strong>
+                  {props.cargo} en <strong>{props.empresa}</strong>
               </p>
-              <p className = 'texo-testimonio'>"{this.props.testimonio}"</p>
+              <p className = 'texo-testimonio'>"{props.testimonio}"</p>
           </div> 
       </div>
-    );
-  }
+  );
 }
 
 export default Testimonio;
